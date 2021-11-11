e72cc62849b7f17c2ec7ead6937941ec9120c2ec
boisset-c <boissetcosme@gmail.com>
2021-11-11 13:27:28 -0600

    working on homepage

---
+++ src/components/common/beacon/Beacon.jsx
@@ -0,0 +1,16 @@
+import { Container, Row, Col, Image } from "react-bootstrap";
+import BeaconDisabled from "../../../assets/gray-btn.svg";
+
+function Beacon() {
+    return(
+        <Container className="py-5" >
+            <Row className="justify-content-center" >
+                <Col xs={5} >
+                <Image src={BeaconDisabled} className="text-center" alt="beacon button" />
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Beacon;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,27 @@
-import {Container}  from "react-bootstrap"
-import NavBarSection from "../../components/common/navbar/NavBarSection"
-import classes from "./Home.module.css"
-
-
+import { Container, Button, Row, Col } from "react-bootstrap";
+import NavBarSection from "../../components/common/navbar/NavBarSection";
+import Beacon from "../../components/common/beacon/Beacon";
+import classes from "./Home.module.css";
 
 function Home() {
-    return(
-       <Container className={classes.bgcustomgray}>
-         <NavBarSection />
-       </Container>
-    )
+  return (
+    <section className={classes.bgcustomgray}>
+      <Container fluid>
+        <NavBarSection />
+        <Beacon />
+      </Container>
+      <Container className="py-5">
+        <Row>
+          <Col xs={12}>
+            <Button variant="dark">Dark</Button>
+          </Col>
+          <Col xs={12}>
+            <Button variant="outline-dark">Dark</Button>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 }
 
 export default Home;
