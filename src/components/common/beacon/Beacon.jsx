3c750bb305a16a1be6b5f234df9e1d884d4190e2
boisset-c <boissetcosme@gmail.com>
2021-11-12 10:21:23 -0600

    Added Homepage

--- src/components/common/beacon/Beacon.jsx
+++ src/components/common/beacon/Beacon.jsx
@@ -2,15 +2,19 @@ import { Container, Row, Col, Image } from "react-bootstrap";
 import BeaconDisabled from "../../../assets/gray-btn.svg";
 
 function Beacon() {
-    return(
-        <Container className="py-5" >
-            <Row className="justify-content-center" >
-                <Col xs={5} >
-                <Image src={BeaconDisabled} className="text-center" alt="beacon button" />
-                </Col>
-            </Row>
-        </Container>
-    )
+  return (
+    <Container className="">
+      <Row className="justify-content-center">
+        <Col xs={5}>
+          <Image
+            src={BeaconDisabled}
+            className="text-center"
+            alt="beacon button"
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Beacon;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,22 +1,28 @@
-import { Container, Button, Row, Col } from "react-bootstrap";
+import { Container, Button, Row, Col, Image } from "react-bootstrap";
 import NavBarSection from "../../components/common/navbar/NavBarSection";
 import Beacon from "../../components/common/beacon/Beacon";
+import PersonIcon from "../../assets/person-icon.svg";
 import classes from "./Home.module.css";
 
 function Home() {
   return (
     <section className={classes.bgcustomgray}>
-      <Container fluid>
+      <Container className="h-25" fluid>
         <NavBarSection />
-        <Beacon />
       </Container>
-      <Container className="py-5">
-        <Row>
-          <Col xs={12}>
-            <Button variant="dark">Dark</Button>
+      <Container className="h-75">
+        <Row className="h-75 align-items-center">
+          <Col>
+            <Beacon />
           </Col>
-          <Col xs={12}>
-            <Button variant="outline-dark">Dark</Button>
+        </Row>
+        <Row className="align-items-end h-25">
+          <Col className="d-grid gap-2">
+            <Button variant="dark">
+              <Image className="px-2" src={PersonIcon} alt="person icon" />
+              ADD YOUR PEOPLE
+            </Button>
+            <Button variant="outline-dark">VIEW YOUR SUPPORT SYSTEM</Button>
           </Col>
         </Row>
       </Container>
